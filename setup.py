8184a736db4bb48a1a555b0112e451ad02a662ce
Stanley He <he.stanley@gmail.com>
2019-06-10 21:25:18 +1000

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis of the California Housing Prices dataset (used in the second chapter of Hands-On Machine learning with Scikit-Learn and TensorFlow)',
+    author='Stanley',
+    license='MIT',
+)
